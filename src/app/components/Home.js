3d0e6a980ee6ad3caeca5b9045ec87d3a975576c
Dominique Reese <dreese@askcts.com>
2017-05-18 15:59:12 -0500

    Making things centered

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -16,14 +16,17 @@ export class Home extends React.Component {
     render() {
         return (
            <div>
-               <h1>This is the Home Component/Page!</h1>
-               <div className="form-group">
-                   <label htmlFor="comment">Comment:</label>
-                   <textarea onChange={this.processText.bind(this)} className="form-control" value={this.state.value} rows="4"> </textarea>
+               <div className="page-header">
+                   <h1>This is the Home Component/Page!</h1>
                </div>
-               <div className="jumbotron">
+               <div style={{"backgroundColor":"purple", "color":"white", "float":"none"}} className="row jumbotron center-block col-lg-6">
                    <p>{this.state.value}</p>
                </div>
+               <div style={{"float":"none"}} className="row form-group col-lg-6 center-block">
+                   <label htmlFor="comment">Comment:</label>
+                   <textarea onChange={this.processText.bind(this)} className="form-control" value={this.state.value} rows="6" > </textarea>
+               </div>
+               {/*<Comments/>          */}
            </div>
         );
     }
